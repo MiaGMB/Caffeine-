05616b805a6757a9874a629776ac2f42ad6478cf
jwernerr <jenny.w0400@gmail.com>
2024-06-27 11:25:20 +0200

    check if user input is valid

---
+++ Connect 4/files/connect4.ts
@@ -0,0 +1,58 @@
+let columns:number=7;
+let rows:number=7;
+window.addEventListener("load",handleLoad);
+
+function handleLoad(_event:Event):void{
+
+    let inputcolumns:number=Number(prompt("Please enter your amount of columns (4-14)","7")!);
+    let inputrows:number=7
+    checkifvalidcolumns();
+
+    function invalidcolumns(){
+        inputcolumns=Number(prompt("Invalid input. Please enter a column amount between 4 and 14.","7")!);
+        checkifvalidcolumns();
+    }
+
+    function checkifvalidcolumns(){
+        if(Number(inputcolumns)<4){
+            invalidcolumns();
+        }
+        else{
+            if (Number(inputcolumns)>14){
+                invalidcolumns();
+            }
+            else{
+               validcolumns();
+            }
+        }
+    }
+
+    function validcolumns(){
+        columns=inputcolumns;
+        inputrows=Number(prompt("Please enter your amount of rows (4-14)","7")!);
+        checkifvalidrows
+    }
+
+    function checkifvalidrows(){
+        if(Number(inputrows)<4){
+            invalidrows();
+        }
+        else{
+            if (Number(inputrows)>14){
+                invalidrows();
+            }
+            else{
+               validrows();
+            }
+        }
+    }
+
+    function invalidrows(){
+        inputrows=Number(prompt("Invalid input. Please enter a row amount between 4 and 14.","7")!);
+        checkifvalidrows();
+    }
+
+    function validrows(){
+        rows=inputrows;
+    }
+}
